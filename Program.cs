3e71ccd8f6fba9b8a60db686fe3a8a9499027a60
tobyagus02 <143465459+tobyagus02@users.noreply.github.com>
2024-04-18 23:52:43 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+using Repository.Data.Facturas;
+using Services.Logica;
+
+namespace PrimerExamenParcial_OptativoIII
+{
+
+    string connectionString = "";
+    private FacturaService facturaService = new FacturaService(connectionString);
+
+    facturaService.add(new FacturaModel
+    
+}
